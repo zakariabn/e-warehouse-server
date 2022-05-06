f3f47b875555e54c8e2c298fc91e053348d0a1c7
Zakaria <zakaria7262@gmail.com>
2022-05-06 15:12:17 +0600

    mongodb config implement and stock get api created

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
 const port = process.env.PORT || 5000;
 
 
@@ -11,11 +12,60 @@ app.use(cors());
 app.use(express.json());
 
 
+// default request
 app.get('/', (req, res) => {
+  console.log('server running');
   res.send('Server running')
 })
+
+// mongodb configuration
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.3rmrc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+// api function
+async function run () {
+
+  try {
+    await client.connect();
+    const database = client.db('warehouseManagement');
+    const stockCollection = database.collection('stock');
+    console.log('Successfully connected to DB');
+
+    // getting stock
+    app.get('/stock', async (req, res) => {
+      const limit = parseInt(req.query.limit);
+      const intChecker = /^[0-9]+$/;
+
+      const query = {};
+      const cursor = stockCollection.find(query);
+      let stock;
+
+      if (intChecker.test(limit)) {
+        stock = await cursor.limit(limit).toArray();
+      }
+      else {
+        stock =  await cursor.toArray();
+      }
+      res.send(stock);
+    })
+  }
+  finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
 
 // listening
 app.listen(port, () => {
   console.log(`Server running on port: ${port}`);
 })
+"initialized"
